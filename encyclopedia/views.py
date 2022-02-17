bc0a621e740854ca86bc40fe5de95a803784ce9e
Macbook Pro <joserodriguez@Joses-MacBook-Pro.local>
2022-02-17 11:47:53 -0800

    first commit

---
+++ encyclopedia/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+from . import util
+
+from markdown2 import Markdown
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def wiki(request, name):
+    # Check to see if the .md file exists
+    mdfile = util.get_entry(name)
+    print("inside wiki")
+    # If so then we convert/write to HTML
+    if(mdfile == None):
+        render("encyclopedia/apology.html")
+
+    print("Passed")
+    markdowner = Markdown()
+    markdowner.convert(mdfile)
+    with open(f'{name}.html', 'w') as f:
+        f.write(markdowner)
+    
+
+    
+    
+
