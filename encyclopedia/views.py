71ef66e7160cdbd21298c4b6dfeb36acbe1e0380
Macbook Pro <joserodriguez@Joses-MacBook-Pro.local>
2022-02-17 17:50:04 -0800

    user input worked using action method

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -11,17 +11,39 @@ def index(request):
 
 def wiki(request, name):
     # Check to see if the .md file exists
+    print("inside wiki")
     mdfile = util.get_entry(name)
-    # If so then we convert/write to HTML
+    # If not an md entry, return error page
     if(mdfile == None):
         return render(request, "encyclopedia/apology.html")
-
+    # convert .md file to html
     markdowner = Markdown()
     html = markdowner.convert(mdfile)
     with open(f'encyclopedia/templates/html/{name}.html', 'w') as f:
         f.write(html)
 
     return render(request, f"html/{name}.html")
+
+def search(request):
+    print("inside search")
+    q = request.GET['q']
+    mdfile = util.get_entry(q)
+    # If not an md entry, return error page
+    if(mdfile == None):
+        return render(request, "encyclopedia/apology.html")
+    # convert .md file to html
+    markdowner = Markdown()
+    html = markdowner.convert(mdfile)
+    with open(f'encyclopedia/templates/html/{q}.html', 'w') as f:
+        f.write(html)
+
+    return render(request, f"html/{q}.html")
+
+def create(request):
+    print("inside create")
+    q = request.GET['q']
+    print(q)
+
     
 
     
